b3685968ce28fe4d4ba6c0ebe2e3e58be7a2e6f1
kokona <kokonajyamuojisann@gmail.com>
2025-12-10 08:10:47 +0900

    Update cursor

---
+++ millennium-script.js
@@ -0,0 +1,16 @@
+// Swiper 設定（手動）
+const swiper = new Swiper(".menucard-slider", {
+  slidesPerView: 3,
+  loop: false,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  breakpoints: {
+    768: { slidesPerView: 3 },
+    480: { slidesPerView: 2 },
+    0:   { slidesPerView: 1 }
+  }
+});
+
+
